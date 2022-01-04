b4babb89cb2225ccfedc2bb69fed488d1876c9f4
utkarsh seth <utkarshseth2019@gmail.com>
2022-01-04 09:51:55 +0530

    pagLinks chnage on hover after div widths

--- components/PageLinks.jsx
+++ components/PageLinks.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function PageLinks() {
   const pageLinksHoverBorder =
-    "relative hover:text-black after:content-[''] after:h-1 after:mt-4 after:w-10 after:bg-gradient-to-r after:from-green-500 after:to-blue-400 hover:after:absolute after:left-0 md:after:top-8 lg:after:top-10 ease-in-out after:duration-800 ";
+    "relative hover:text-black after:content-[''] after:h-1 after:mt-4 after:w-12 after:bg-gradient-to-r after:from-green-500 after:to-blue-400 hover:after:absolute after:left-0 md:after:top-8 lg:after:top-10 ease-in-out after:duration-800 ";
   return (
     <>
       <div className="flex gap-8 text-gray-400  hidden md:flex ">
@@ -10,16 +10,16 @@ function PageLinks() {
           <a className={pageLinksHoverBorder}>Home</a>
         </Link>
         <Link href="/">
-          <a className={pageLinksHoverBorder}>About</a>
+          <a className={pageLinksHoverBorder + `after:w-12`}>About</a>
         </Link>
         <Link href="/">
-          <a className={pageLinksHoverBorder}>Contact</a>
+          <a className={pageLinksHoverBorder + `after:w-16`}>Contact</a>
         </Link>
         <Link href="/">
           <a className={pageLinksHoverBorder}>Blog</a>
         </Link>
         <Link href="/">
-          <a className={pageLinksHoverBorder}>Careers</a>
+          <a className={pageLinksHoverBorder + `after:w-16`}>Careers</a>
         </Link>
       </div>
     </>
