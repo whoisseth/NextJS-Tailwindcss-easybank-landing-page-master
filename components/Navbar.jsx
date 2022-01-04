e84d824a290ac7f099dc74c219bb84898a77d615
utkarsh seth <utkarshseth2019@gmail.com>
2022-01-04 18:16:13 +0530

    navbar menu responsive complete

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,16 +4,38 @@ import PageLinks from "./PageLinks";
 import Logo from "./Logo";
 import { MenuIcon, Close } from "@heroicons/react/outline";
 import { CgClose } from "react-icons/cg";
+import { useState } from "react";
 
 function Navbar() {
+  const [showMe, setShowMe] = useState(true);
+  function toggle() {
+    setShowMe(!showMe);
+  }
   return (
     <>
-      <div className="flex justify-between py-6 px-6 md:px-28  items-center border">
+      <div className="flex  justify-between py-6 px-6 md:px-28  items-center border">
         <Logo />
-        <PageLinks />
+        <PageLinks display={`hidden`} />
         <BtnRequestInvite />
-        <MenuIcon className="h-8 md:hidden cursor-pointer  " />
-        {/* <CgClose className=" w-8 h-8 md:hidden cursor-pointer  " /> */}
+        {showMe ? (
+          <MenuIcon
+            onClick={toggle}
+            className="h-8 md:hidden cursor-pointer transition ease-in-out delay-300  "
+          />
+        ) : (
+          <div>
+            <CgClose
+              onClick={toggle}
+              className="relative  w-8 h-8 md:hidden cursor-pointer transition ease-in-out delay-300  "
+            />
+            <div className="flex justify-center items-center mx-8   absolute left-0 right-0 top-[7rem]  content-center">
+              <PageLinks
+                display={`flex flex-col  bg-white  md:hidden  items-center rounded-md  border mx-auto gap-4 py-4 w-[30rem] bg:black`}
+                link={`text-black after:content-none`}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
--- components/PageLinks.jsx
+++ components/PageLinks.jsx
@@ -1,28 +1,25 @@
 import Link from "next/link";
 
-function PageLinks() {
-  const pageLinksHoverBorder =
-    "relative hover:text-black after:content-[''] after:h-1 after:mt-4 after:w-12 after:bg-gradient-to-r after:from-green-500 after:to-blue-400 hover:after:absolute after:left-0 md:after:top-8 lg:after:top-10 ease-in-out after:duration-800 ";
+function PageLinks({ display, link }) {
+  const pageLinksHoverBorder = `relative hover:text-black after:content-[''] after:h-1 after:mt-4 after:w-12 after:bg-gradient-to-r after:from-green-500 after:to-blue-400 hover:after:absolute after:left-0 md:after:top-8 lg:after:top-10 ease-in-out after:duration-800 `;
   return (
-    <>
-      <div className="flex gap-8 text-gray-400  hidden md:flex ">
-        <Link href="/">
-          <a className={pageLinksHoverBorder}>Home</a>
-        </Link>
-        <Link href="/">
-          <a className={pageLinksHoverBorder + `after:w-12`}>About</a>
-        </Link>
-        <Link href="/">
-          <a className={pageLinksHoverBorder + `after:w-16`}>Contact</a>
-        </Link>
-        <Link href="/">
-          <a className={pageLinksHoverBorder}>Blog</a>
-        </Link>
-        <Link href="/">
-          <a className={pageLinksHoverBorder + `after:w-16`}>Careers</a>
-        </Link>
-      </div>
-    </>
+    <div className={`  ${display} md:flex gap-8 text-gray-400 `}>
+      <Link href="/">
+        <a className={pageLinksHoverBorder + link}>Home</a>
+      </Link>
+      <Link href="/">
+        <a className={pageLinksHoverBorder + `after:w-12 ` + link}>About</a>
+      </Link>
+      <Link href="/">
+        <a className={pageLinksHoverBorder + `after:w-16 ` + link}>Contact</a>
+      </Link>
+      <Link href="/">
+        <a className={pageLinksHoverBorder + `after:w-9 ` + link}>Blog</a>
+      </Link>
+      <Link href="/">
+        <a className={pageLinksHoverBorder + `after:w-16 ` + link}>Careers</a>
+      </Link>
+    </div>
   );
 }
 
